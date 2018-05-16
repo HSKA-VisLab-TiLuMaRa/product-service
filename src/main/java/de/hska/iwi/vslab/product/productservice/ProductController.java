b9e322200352491efc8f70c128e888228d410e05
Lukas Kugler <lukikugler@gmail.com>
2018-05-16 10:18:39 +0200

    added Params name and categoryId to /Products (s. Postman Collection)

--- src/main/java/de/hska/iwi/vslab/product/productservice/ProductController.java
+++ src/main/java/de/hska/iwi/vslab/product/productservice/ProductController.java
@@ -2,6 +2,7 @@
 package de.hska.iwi.vslab.product.productservice;
 
 import java.net.URI;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -21,8 +23,15 @@ public class ProductController {
 	private ProductRepo repo;
 
 	@RequestMapping(value = "/products", method = RequestMethod.GET)
-	public ResponseEntity<Iterable<Product>> getProducts() {
-		Iterable<Product> allPolls = repo.findAll();
+	public ResponseEntity<Iterable<Product>> getProducts(@RequestParam Optional<String> name, @RequestParam Optional<Integer> categoryId) {
+		Iterable<Product> allPolls;
+		if (name.isPresent()){
+			allPolls = repo.findProductsByName(name.get());
+		} else if (categoryId.isPresent()) {
+			allPolls = repo.findProductsByCategory(categoryId.get());
+		} else {
+			allPolls = repo.findAll();
+		}    
 		return new ResponseEntity<Iterable<Product>>(allPolls, HttpStatus.OK);
 	}
 
--- src/main/java/de/hska/iwi/vslab/product/productservice/ProductRepo.java
+++ src/main/java/de/hska/iwi/vslab/product/productservice/ProductRepo.java
@@ -1,3 +1,12 @@
 package de.hska.iwi.vslab.product.productservice;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-public interface ProductRepo extends CrudRepository<Product, Long> { }
+import org.springframework.data.repository.query.Param;
+
+public interface ProductRepo extends CrudRepository<Product, Long> { 
+    @Query("SELECT t FROM Product t where t.name = :name") 
+    Iterable<Product> findProductsByName(@Param("name") String name);
+
+    @Query("SELECT t FROM Product t where t.categoryId = :categoryId") 
+    Iterable<Product> findProductsByCategory(@Param("categoryId") Integer categoryId);
+}
